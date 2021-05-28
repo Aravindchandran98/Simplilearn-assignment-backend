533c77eb4d55404524f6315fdd89773c445bcbfe
Aravindchandran98 <aravindkumar04016@gmail.com>
2021-05-28 14:30:16 +0530

    ssl false

--- config/db.js
+++ config/db.js
@@ -1,15 +1,15 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  // ssl: {
-  // 	rejectUnauthorized: false
-  // }
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool
-.connect()
-.then(() => console.log('Database Connected'))
-.catch(err => console.error('Error Connecting to DB: ', err.stack));
+  .connect()
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.error("Error Connecting to DB: ", err.stack));
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
